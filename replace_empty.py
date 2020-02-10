747174d2af980d2a42e0be502bc858475c5e3a5d
Guanchen <guanchen.song.797@gmail.com>
2020-02-10 21:20:06 +0100

    Add script

---
+++ replace_empty.py
@@ -0,0 +1,126 @@
+import argparse
+
+import numpy
+import pandas as pd
+
+
+def replace_empty(dataframe):
+    for column in dataframe.columns:
+        try:
+            dataframe.loc[
+                dataframe[column].str.strip().str.len() == 0,
+                column
+            ] = numpy.nan
+        except AttributeError:
+            pass
+
+    return dataframe
+
+
+def replace_empty_by_columns(dataframe, columns):
+    for column in columns:
+        try:
+            dataframe.loc[
+                dataframe[column].str.strip().str.len() == 0,
+                column
+            ] = numpy.nan
+        except AttributeError:
+            pass
+
+    return dataframe
+
+
+def replace_empty_by_values(dataframe, values):
+    value = values[0]
+    for column in dataframe.columns:
+        try:
+            dataframe.loc[
+                dataframe[column].str.strip().str.len() == 0,
+                column
+            ] = value
+        except AttributeError:
+            pass
+
+    return dataframe
+
+
+def replace_empty_by_columns_values(dataframe, columns, values):
+    if len(columns) == len(values):
+        lenght = len(columns)
+        for i in range(lenght):
+            column = columns[i]
+            value = values[i]
+            try:
+                dataframe.loc[
+                    dataframe[column].str.strip().str.len() == 0,
+                    column
+                ] = value
+            except AttributeError:
+                pass
+    else:
+        value = values[0]
+        for column in columns:
+            try:
+                dataframe.loc[
+                    dataframe[column].str.strip().str.len() == 0,
+                    column
+                ] = value
+            except AttributeError:
+                pass
+
+    return dataframe
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    # Mandatory arguments
+    parser.add_argument("data", help='Data file')
+    # Optional arguments
+    parser.add_argument(
+        "-c",
+        "--columns",
+        nargs='+',
+        help='Column(s) to replace [-c Column1 Column2]'
+    )
+    parser.add_argument("--sep", help='Separator', default='|')
+    parser.add_argument(
+        "-v",
+        "--values",
+        nargs='+',
+        help='Replace with the new value(s) [-v valueForC1 valueForC2]'
+    )
+    args = parser.parse_args()
+
+    data_file = args.data
+    data_dataframe = pd.read_csv(data_file, sep=args.sep)
+
+    # Check optionnal arguments
+    if not args.columns and not args.values:
+        new_dataframe = replace_empty(data_dataframe)
+
+    if args.columns and not args.values:
+        new_dataframe = replace_empty_by_columns(data_dataframe, args.columns)
+
+    if not args.columns and args.values:
+        if len(args.values) != 1:
+            print("Too many values")
+            exit()
+        else:
+            new_dataframe = replace_empty_by_values(
+                                data_dataframe, args.values)
+
+    if args.columns and args.values:
+        if len(args.columns) != len(args.values) and len(args.values) != 1:
+            print("Too many values")
+            exit()
+        else:
+            new_dataframe = replace_empty_by_columns_values(
+                                data_dataframe, args.columns,
+                                args.values)
+
+    print(new_dataframe)
+
+
+# Define what to do if file is run as script
+if __name__ == "__main__":
+    main()
